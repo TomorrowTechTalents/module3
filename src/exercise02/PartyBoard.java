2d5bc654d9d399e780109155297587252a330116
marcmam2 <marcmam2@gmail.com>
2022-05-31 23:24:49 -0300

    fix the checking if there is a winner in a partyboard-based game

--- src/exercise02/PartyBoard.java
+++ src/exercise02/PartyBoard.java
@@ -1,13 +1,17 @@
 package exercise02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PartyBoard implements Board {
+    private static final byte NUMBER_OF_PLAYERS = 5;
     private Card[] deck1 = new Card[80];
     Card[] deck2 = new Card[80];
     Card[] deck3 = new Card[80];
     Card[] deck4 = new Card[80];
     Card[] deck5 = new Card[80];
 
-    private int numberOfAttackCardsOnBoard;
+    private int numberOfAttackCardsOnBoard = 0;
 
     @Override
     public void insertDeck(Card[] deck) {
@@ -16,19 +20,33 @@ public class PartyBoard implements Board {
 
     @Override
     public void receiveAttackCard(Card attackCard) {
-        if (numberOfAttackCardsOnBoard >= 34) {
+        if (this.numberOfAttackCardsOnBoard >= 34) {
             System.out.println("não cabe mais carta de ataque no tabuleiro");
+        } else {
+            this.numberOfAttackCardsOnBoard++;
         }
     }
 
     @Override
     public void checkIfThereIsAWinner(Player[] players) {
-        for (Player player : players) {
-            if (player.getLifePoints() > 0) {
-                return;
+
+        List<Integer> indexesOfOperationalPlayers = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
+            if (players[i].getLifePoints() > 0) {
+                indexesOfOperationalPlayers.add(i);
             }
         }
 
-        System.out.println("Parabéns!!");
+        if (indexesOfOperationalPlayers.size() == 1) {
+            System.out.println("Parabéns ao jogador " + indexesOfOperationalPlayers.get(0) + 1 + "!!");
+            System.out.println("Mais sorte aos demais jogadores!!!");
+
+            return;
+        }
+
+        if (indexesOfOperationalPlayers.size() < 1) {
+            System.out.println("empate");
+        }
     }
 }
